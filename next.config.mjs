1f656a1d84322cfccb926e990adf21ce4208a22b
Yessine Ben Fraj <your.github.email@example.com>
2026-08-19 15:17:04 +0100

    Installed lucide-react icons and cleaned config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // This allows the production build to successfully complete even if
-    // your project has ESLint errors (like French apostrophes).
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
